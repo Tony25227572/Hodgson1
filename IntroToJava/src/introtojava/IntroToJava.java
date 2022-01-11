c9174c568b44e4fef921d60b9a8799b5a0137c34
Hodgson <M2104721@middlesbro.ac.uk>
2022-01-11 11:25:03 +0000

    first commit

---
+++ IntroToJava/src/introtojava/IntroToJava.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package introtojava;
+
+/**
+ *
+ * @author m2104721
+ */
+public class IntroToJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int num1 =32;
+        System.out.println("my age is "+num1);
+    }
+    
+}// do not write beyond this point.
